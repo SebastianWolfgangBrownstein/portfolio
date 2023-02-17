9e4aeb8a8269ad0eb7c82a243fbbf4ab71b9c90c
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-02-17 15:26:08 -0700

    Added content to Protovibes feature and basic layout

--- components/MyWork.jsx
+++ components/MyWork.jsx
@@ -2,20 +2,86 @@
 
 const MyWork = () => {
   return (
-    <section className="w-full h-200 bg-oxfordblue text-center px-150 py-36">
-        <h2>My Recent Work</h2>
-        <div>
-            <h3>ProtoVibes</h3>
+    <section className="w-full bg-oxfordblue text-center px-60 py-10">
+        <h2 className="text-3xl font-secular text-ivory mb-10">My Recent Work</h2>
+        <div className="border border-mint grid grid-cols-2">
+            <div>
+
+            </div>
+            <div>
+                <div className="text-right">
+                    <span className="text-sm text-mint">Featured Project</span>
+                    <h3 className="font-secular text-3xl">ProtoVibes</h3>
+                </div>
+                <div className="text-right">
+                    <p>A web tool for converting video files to raw audio. Achieved 10-15% faster conversions than alternatives while allowing the user full control over the file&apos;s metadata, output format, and audio quality.</p>
+                </div>
+                <div className="">
+                    <ul className="flex justify-end space-x-3 text-right">
+                        <li>NextJS</li>
+                        <li>Typescript</li>
+                        <li>Python</li>
+                        <li>Flask</li>
+                        <li>Sass</li>
+                        <li>MySQL</li>
+                        <li>Jest</li>
+                        <li>ffmpeg</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-        <div>
-            <h3>ShadowBox</h3>
+        {/* <div className="border border-mint grid grid-cols-2">
+            <div>
+                <div>
+                    <span>Featured Project</span>
+                    <h3>ShadowBox</h3>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni qui minus cumque impedit enim dignissimos repellat quidem excepturi, minima nobis, reprehenderit hic nam alias? Incidunt quibusdam saepe aspernatur iste ea.</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>NextJS</li>
+                        <li>Typescript</li>
+                        <li>Python</li>
+                        <li>Flask</li>
+                        <li>Sass</li>
+                        <li>MySQL</li>
+                        <li>Jest</li>
+                        <li>ffmpeg</li>
+                    </ul>
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
-        <div>
-            <h3>Divine Mud</h3>
-        </div>
-        <div>
-            <h3>Personal Website</h3>
-        </div>
+        <div className="border border-mint grid grid-cols-2">
+            <div>
+
+            </div>
+            <div>
+                <div>
+                    <span>Featured Project</span>
+                    <h3>Divine Mud</h3>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni qui minus cumque impedit enim dignissimos repellat quidem excepturi, minima nobis, reprehenderit hic nam alias? Incidunt quibusdam saepe aspernatur iste ea.</p>
+                </div>
+                <div>
+                    <ul>
+                        <li>NextJS</li>
+                        <li>Typescript</li>
+                        <li>Python</li>
+                        <li>Flask</li>
+                        <li>Sass</li>
+                        <li>MySQL</li>
+                        <li>Jest</li>
+                        <li>ffmpeg</li>
+                    </ul>
+                </div>
+            </div>
+        </div> */}
     </section>
   )
 }
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -12,16 +12,16 @@ const Skills = () => {
                 </div>
                 
                 <h2 className="text-2xl font-secular mb-5 text-ivory">System Architect</h2>
-                <p className="mb-10 text-lg">I care about designing systems that are durable, evolvable, and scalable before ever writing a single line of code. </p>
+                <p className="mb-10 text-lg text-ivory">I care about designing systems that are durable, evolvable, and scalable before ever writing a single line of code. </p>
 
                 <div className="mb-10">
                     <h3 className="mb-2 text-amethyst">Things I enjoy designing:</h3>
-                    <p>Microservices, Interfaces, Modules, Libraries, Packages</p>
+                    <p className="text-ivory">Microservices, Interfaces, Modules, Libraries, Packages</p>
                 </div>
 
                 <div>
                     <h3 className="mb-2 text-amethyst">Design Tools:</h3>
-                    <ul>
+                    <ul className="text-ivory">
                         <li className="mb-1">Schemas</li>
                         <li className="mb-1">User Flows</li>
                         <li className="mb-1">Sequence Diagrams</li>
@@ -34,20 +34,20 @@ const Skills = () => {
             <div className="py-16 px-8">
                 <div className="flex justify-center mb-6">
                     <div className="relative w-20 h-20 border border-mint rounded-full bg-mint">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute left-4 top-3" height="48" viewBox="0 96 960 960" width="48"><path d="M320 814 80 574l242-242 43 43-199 199 197 197-43 43Zm318 2-43-43 199-199-197-197 43-43 240 240-242 242Z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute left-4 top-3.5" height="48" viewBox="0 96 960 960" width="48"><path d="M320 814 80 574l242-242 43 43-199 199 197 197-43 43Zm318 2-43-43 199-199-197-197 43-43 240 240-242 242Z"/></svg>
                     </div>
                 </div>
                 <h2 className="text-2xl font-secular mb-5 text-ivory">Backend Developer</h2>
-                <p className="mb-10 text-lg">I love building powerful tools and I value clean API design with an emphasis on usability, encapsulation, and security.</p>
+                <p className="mb-10 text-lg text-ivory">I love building powerful tools and I value clean API design with an emphasis on usability, encapsulation, and security.</p>
 
                 <div className="mb-10">
                     <h3 className="mb-2 text-amethyst">Frameworks & protocols I use:</h3>
-                    <p>Node.js, Flask, REST, RPC, SSH, OpenAPI</p>
+                    <p className="text-ivory">Node.js, Flask, REST, RPC, SSH, OpenAPI</p>
                 </div>
 
                 <div>
                     <h3 className="mb-2 text-amethyst">Tech & Dev Tools:</h3>
-                    <ul>
+                    <ul className="text-ivory">
                         <li className="mb-1">Typescript</li>
                         <li className="mb-1">Python</li>
                         <li className="mb-1">Express</li>
@@ -68,21 +68,21 @@ const Skills = () => {
             <div className="py-16 px-8">
                 <div className="flex justify-center mb-6">
                     <div className="relative w-20 h-20 border border-mint rounded-full bg-mint">
-                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute left-4 top-3" height="48" viewBox="0 96 960 960" width="48"><path d="M583 1016H440q-14.45 0-24.225-9.78Q406 996.45 406 982V732q0-14.45 9.775-24.225Q425.55 698 440 698h41V565H140q-24.75 0-42.375-17.625T80 505V312q0-24.75 17.625-42.375T140 252h83v-42q0-14.45 9.775-24.225Q242.55 176 257 176h509q14.45 0 24.225 9.775Q800 195.55 800 210v152q0 14.45-9.775 24.225Q780.45 396 766 396H257q-14.45 0-24.225-9.775Q223 376.45 223 362v-50h-83v193h341q24.75 0 42.375 17.625T541 565v133h42q14.45 0 24.225 9.775Q617 717.55 617 732v250q0 14.45-9.775 24.22Q597.45 1016 583 1016ZM283 236v100-100Zm183 720h91V758h-91v198ZM283 336h457V236H283v100Zm183 620h91-91Z"/></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="absolute left-4 top-3.5" height="48" viewBox="0 96 960 960" width="48"><path d="M583 1016H440q-14.45 0-24.225-9.78Q406 996.45 406 982V732q0-14.45 9.775-24.225Q425.55 698 440 698h41V565H140q-24.75 0-42.375-17.625T80 505V312q0-24.75 17.625-42.375T140 252h83v-42q0-14.45 9.775-24.225Q242.55 176 257 176h509q14.45 0 24.225 9.775Q800 195.55 800 210v152q0 14.45-9.775 24.225Q780.45 396 766 396H257q-14.45 0-24.225-9.775Q223 376.45 223 362v-50h-83v193h341q24.75 0 42.375 17.625T541 565v133h42q14.45 0 24.225 9.775Q617 717.55 617 732v250q0 14.45-9.775 24.22Q597.45 1016 583 1016ZM283 236v100-100Zm183 720h91V758h-91v198ZM283 336h457V236H283v100Zm183 620h91-91Z"/></svg>
                     </div>
                 </div>
                 
                 <h2 className="text-2xl font-secular mb-5 text-ivory">Frontend Developer</h2>
-                <p className="mb-10 text-lg">I&apos;m passionate about bringing complex tools to life through visually striking user interfaces that are equal parts elegant & intuitive.</p>
+                <p className="mb-10 text-lg text-ivory">I&apos;m passionate about bringing complex tools to life through visually striking user interfaces that are equal parts elegant & intuitive.</p>
 
                 <div className="mb-10">
                     <h3 className="mb-2 text-amethyst">Languages I speak:</h3>
-                    <p>Javascript, HTML, CSS, Sass</p>
+                    <p className="text-ivory">Javascript, HTML, CSS, Sass</p>
                 </div>
 
                 <div>
                     <h3 className="mb-2 text-amethyst">Tech & Dev Tools:</h3>
-                    <ul>
+                    <ul className="text-ivory">
                         <li className="mb-1">ReactJS</li>
                         <li className="mb-1">NextJS</li>
                         <li className="mb-1">RxJS</li>
