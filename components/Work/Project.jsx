2b8b2adac52d92b404bdd7821b18c91e1936db2d
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-02-27 02:28:43 -0700

    added responsive styling to Project component

--- components/Work/Project.jsx
+++ components/Work/Project.jsx
@@ -11,25 +11,25 @@ const Project = ({
 
     return (
         <li className="grid grid-cols-12">
-            <div className="bg-midnight-100 col-start-1 col-end-8 row-start-1 row-end-[-1] relative h-96 w-full rounded-md overflow-hidden">
-                <Image className="opacity-60 transition-all duration-300 ease-linear hover:opacity-100" src={imagePath} fill alt={imageAlt}/>
+            <div className="bg-oxfordblue lg:bg-midnight-100 col-start-1 col-end-13 lg:col-start-1 lg:col-end-8 row-start-1 row-end-[-1] relative h-96 w-full rounded-md overflow-hidden">
+                <Image className="opacity-10 lg:opacity-60 transition-all duration-300 ease-linear lg:hover:opacity-100" src={imagePath} fill alt={imageAlt}/>
             </div>
-            <div className="col-start-7 col-end-[-1] row-start-1 row-end-[-1] z-10 pt-7 pointer-events-none">
-                <div className="text-right">
-                    <p className="text-sm text-mint my-2.5">Featured Project</p>
-                    <h3 className="font-secular text-lightgray text-3xl mb-5">{name}</h3>
+            <div className="col-start-1 col-end-13 lg:col-start-7 lg:col-end-[-1] row-start-1 row-end-[-1] z-10 pt-7 pointer-events-none">
+                <div className="text-left lg:text-right pl-4 lg:pl-0">
+                    <p className="text-sm text-mint my-1 lg:my-2.5">Featured Project</p>
+                    <h3 className="font-secular text-lightgray text-3xl mb-2 lg:mb-5">{name}</h3>
                 </div>
-                <div className="text-right bg-grayblue p-6 mb-5 rounded-md shadow-md">
+                <div className="text-left lg:text-right lg:bg-grayblue p-6 mb-5 rounded-md lg:shadow-md">
                     <p className="text-lightgray">{description}</p>
                 </div>
                 <div className="">
-                    <ul className="flex justify-end space-x-3 text-right">
+                    <ul className="flex flex-wrap lg:flex-nowrap lg:justify-end space-x-3 text-right">
                         {technologies.map((tech, i) => (
                             <li 
                                 key={i}
                                 className={`
                                 border border-${tech} text-${tech} font-genos text-xl
-                                bg-overlay-400 rounded-full px-3 pb-1`}
+                                bg-overlay-400 rounded-full px-3 pb-1 mb-4`}
                             >{tech === 'ffmpeg' ? tech : capitalizeFirstLetter(tech)}</li>
                         ))}
                     </ul>
