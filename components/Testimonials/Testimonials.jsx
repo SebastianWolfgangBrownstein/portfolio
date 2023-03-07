aeebe0421919b99906545ce206821897d6bab207
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-03-07 10:05:58 -0700

    carousel background changed

--- components/Testimonials/Testimonials.jsx
+++ components/Testimonials/Testimonials.jsx
@@ -3,7 +3,7 @@ import TestimonialSlider from "./TestimonialSlider"
 
 const Testimonials = () => {
   return (
-    <section className="w-full bg-byzantine-darkest text-center px-6 py-36  xl:px-150 xl:py-36">
+    <section className="w-full bg-gunmetal-dark text-center px-6 py-36  xl:px-150 xl:py-36">
         <h2 className="text-3xl font-play mb-5 text-gold">Testimonials</h2>
         <p className="text-xl text-tan mb-16 xl:mb-16">People I&apos;ve worked with have said some nice things...</p>
         
