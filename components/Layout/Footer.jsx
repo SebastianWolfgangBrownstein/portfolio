9e0e18af473ebfd620501bfcfb96eb751f066a40
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-03-07 21:10:13 -0700

    added home link to footer logo

--- components/Layout/Footer.jsx
+++ components/Layout/Footer.jsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+
 import {
     GithubIcon,
     LinkedinIcon,
@@ -10,12 +12,15 @@ export const Footer = () => {
     return (
         <footer className="flex flex-col items-center w-full text-center pb-28" style={{backgroundImage: "radial-gradient(circle at center, #870000, #190a05, #190a05)"}}>
             <LetsChat />
-
-            <div className="mt-32 mb-10">
-                <h1 className="text-6xl font-genos text-gold">SEB</h1>
-            </div>
+            
+            <Link href="/">
+                <div className="mt-32 mb-10">
+                    <h1 className="text-6xl font-genos text-gold">SEB</h1>
+                </div>
+            </Link>
+            
             <div className="w-64 mb-24 lg:w-1/6 lg:px-10 lg:mb-14">
-                <p className="text-xl text-tan">Living, learning, & leveling up one day at a time.</p>
+                <p className="text-xl font-play text-tan-150 tracking-wider">Vengo, Vedo, Vinco</p>
             </div>
             <div className="flex space-x-5 mb-16">
                 <a href="https://www.linkedin.com/in/swolfgangb/" rel="noreferrer noopener" target="_blank">
