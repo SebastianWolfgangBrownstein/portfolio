37a66d7d0c2ffd0165bc14badec99ca2ebba4ef5
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-03-08 04:48:38 -0700

    added glitch effect to hero shot image

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,14 +1,13 @@
-import Image from "next/image";
+
+import HeroShot from './HeroShot'
 
 export const Hero = () => {
     return (
         <section className="relative w-full lg:h-screen pb-10 pt-32" style={{backgroundImage: "radial-gradient(circle at center, #870000, #190a05, #190a05)"}}>  
-            <div className="z-10 flex justify-center mb-5">
-                <Image className="sepia z-10 ml-8" src="/images/TorsoShot.png" width={350} height={800} quality={100} priority={true} alt="Seb"/>
-            </div>
+            <HeroShot />
             <div className="z-20 text-center -mt-4">
                 {/* <h1 className="text-6xl mb-6 font-genos bg-gradient-to-r from-tan via-gold to-tan bg-clip-text text-transparent">SEBASTIAN WOLFGANG BROWNSTEIN</h1> */}
-                <h1 className="text-5xl mb-6 font-genos bg-gradient-to-r from-tan via-gold to-tan bg-clip-text text-transparent">FULL STACK WEB DEVELOPER</h1>
+                <h1 className="text-4xl md:text-5xl mb-6 font-genos bg-gradient-to-r from-tan via-gold to-tan bg-clip-text text-transparent">FULL STACK WEB DEVELOPER</h1>
             </div>
         </section>
     );
--- components/Hero/HeroShot.jsx
+++ components/Hero/HeroShot.jsx
@@ -1,9 +1,39 @@
+import { useState, useEffect } from 'react';
 import classes from './HeroShot.module.css';
+import Image from "next/image";
 
 const HeroShot = () => {
+    const [glitching, setGlitching] = useState(false);
+
+    useEffect(() => {
+
+        const toggleGlitch = setInterval(() => {
+            setGlitching(!glitching)
+        }, 6000)
+
+        return () => {
+            clearInterval(toggleGlitch)
+        }
+    }, [glitching])
+    
     return (
-        <></>
+        <div className="z-10 flex justify-center mb-5">
+            <div className={classes.glitch}>
+                <Image src='/images/HeroShotSepia.png' width={350} height={800} quality={100} alt="Image of Sebastian Brownstein" priority={true}/>
+                
+                
+                <div className={glitching ? classes.glitchLayers : classes.glitchHidden}>
+                    <div className={classes.glitchLayer}></div>
+                    <div className={classes.glitchLayer}></div>
+                    <div className={classes.glitchLayer}></div>
+                </div>
+            </div>
+        </div>
     )
 }
 
 export default HeroShot;
+
+{/* <div className="z-10 flex justify-center mb-5">
+            <Image className="sepia z-10 ml-8" src="/images/TorsoShot.png" width={350} height={800} quality={100} priority={true} alt="Seb"/>
+        </div> */}
