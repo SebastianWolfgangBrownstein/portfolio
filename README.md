01e4f0c2f1e3faa0ab393b2bf2afbd5e39f1a7ca
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-03-07 23:36:34 -0700

    changed font-display from optional to swap for enforced font loading

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 <h1 align="center">My Personal Portfolio Webpage </h1>
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4c9098cd-64e7-4c6d-ac3f-79e9e2fa176b/deploy-status)](https://app.netlify.com/sites/delicate-toffee-073890/deploys)
 
 ### Created with:
 - [Next.js](https://nextjs.org/)
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@0,400;0,500;1,400;1,500&display=optional" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=optional" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=optional" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Genos:ital,wght@0,400;0,500;1,400;1,500&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Secular+One&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Play:wght@400;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
