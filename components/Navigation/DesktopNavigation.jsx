cf52b6e2e74f15cc9ba2e5c8a388eefe59fe529b
SebastianBrownstein <sbrownstein.dev@gmail.com>
2023-03-29 22:13:39 -0600

    added layout effect to mobile nav to properly handle animation cleanup

--- components/Navigation/DesktopNavigation.jsx
+++ components/Navigation/DesktopNavigation.jsx
@@ -1,12 +1,19 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useLayoutEffect, useRef } from 'react';
 import { gsap, ScrollTrigger } from '../../config/gsapConfig';
 import classes from './Navigation.module.css';
 import NavItem from './NavItem';
 
 
 const DesktopNavigation = () => {
+    const scopeRef = useRef();
     const navRef = useRef(null);
     const [active, setActive] = useState(0);
+
+    const aboutRef = useRef(null);
+    const workRef = useRef(null);
+    const experienceRef = useRef(null);
+    const testimonialsRef = useRef(null);
+    const contactRef = useRef(null);
 
 
     useEffect(() => {
@@ -17,7 +24,7 @@ const DesktopNavigation = () => {
         const workTab = lbs[1];
         const experienceTab = lbs[2];
         const testimonialsTab = lbs[3];
-        const contactTab = lbs[4]
+        const contactTab = lbs[4];
 
         /* About Tab Animation */
         gsap.to(aboutTab, {
@@ -75,7 +82,6 @@ const DesktopNavigation = () => {
 
         /* Testimonials Tab Animation */
         gsap.to(testimonialsTab, {
-            duration: 0.6,
             y: -40,
             ease: "bounce.out",
             delay: 0.4,
@@ -129,17 +135,17 @@ const DesktopNavigation = () => {
         /* Show/Hide Animation */
 
         const showNav = gsap.fromTo(navRef.current, {
-                    opacity: 0,
-                    yPercent: -100
-                },
-                {
-                    opacity: 1,
-                    yPercent: 0,
-                    duration: 0.3,
-                    paused: true
-                }
-                ).progress(1)
-        
+                opacity: 0,
+                yPercent: -100
+            },
+            {
+                opacity: 1,
+                yPercent: 0,
+                duration: 0.3,
+                paused: true
+            }
+        ).progress(1)
+
         ScrollTrigger.create({
             trigger: document.querySelector('#skills'),
             start: "top 30%",
@@ -148,52 +154,53 @@ const DesktopNavigation = () => {
                 self.direction === -1 ? showNav.play() : showNav.reverse()
             }
         });
-
     }, [])
 
     return (
-        <div ref={navRef} className={classes.header} >
-            <nav className={classes.menu}>
-                <ul>
-                    <NavItem 
-                        index={0}
-                        isActive={active}
-                        label="About"
-                        linkId="about"
-                    />
-                    <NavItem 
-                        index={1}
-                        isActive={active}
-                        label="Work"
-                        linkId="work"
-                    />
-                    <NavItem 
-                        index={2}
-                        isActive={active}
-                        label="Experience"
-                        linkId="experience"
-                    />
-                    <NavItem 
-                        index={3}
-                        isActive={active}
-                        label="Testimonials"
-                        linkId="testimonials"
-                    />
-                    <NavItem 
-                        index={4}
-                        isActive={active}
-                        label="Contact"
-                        linkId="contact"
-                    />
-                </ul>
-                <svg className={classes.lineBottom} width="1000" height="30" viewBox="0,0 1350,40">
-                    <polygon className="lb" points="43,53 123,53 133,43 143,53 223,53" />
-                    <polygon className="lb" points="313,53 393,53 403,43 413,53 493,53" />
-                    <polygon className="lb" points="583,53 663,53 673,43 683,53 761,53" />
-                    <polygon className="lb" points="853,53 933,53 943,43 953,53 1033,53" />
-                    <polygon className="lb" points="1123,53 1203,53 1213,43 1223,53 1303,53" />
-                </svg>
-            </nav>
+        <div ref={scopeRef}>
+            <div ref={navRef} className={classes.header} >
+                <nav className={classes.menu}>
+                    <ul>
+                        <NavItem 
+                            index={0}
+                            isActive={active}
+                            label="About"
+                            linkId="about"
+                        />
+                        <NavItem 
+                            index={1}
+                            isActive={active}
+                            label="Work"
+                            linkId="work"
+                        />
+                        <NavItem 
+                            index={2}
+                            isActive={active}
+                            label="Experience"
+                            linkId="experience"
+                        />
+                        <NavItem 
+                            index={3}
+                            isActive={active}
+                            label="Testimonials"
+                            linkId="testimonials"
+                        />
+                        <NavItem 
+                            index={4}
+                            isActive={active}
+                            label="Contact"
+                            linkId="contact"
+                        />
+                    </ul>
+                    <svg className={classes.lineBottom} width="1000" height="30" viewBox="0,0 1350,40">
+                        <polygon ref={aboutRef} className="lb" points="43,53 123,53 133,43 143,53 223,53" />
+                        <polygon ref={workRef} className="lb" points="313,53 393,53 403,43 413,53 493,53" />
+                        <polygon ref={experienceRef} className="lb" points="583,53 663,53 673,43 683,53 761,53" />
+                        <polygon ref={testimonialsRef} className="lb" points="853,53 933,53 943,43 953,53 1033,53" />
+                        <polygon ref={contactRef} className="lb" points="1123,53 1203,53 1213,43 1223,53 1303,53" />
+                    </svg>
+                </nav>
+            </div>
         </div>
     )
 }
--- components/Navigation/MobileNavigation.jsx
+++ components/Navigation/MobileNavigation.jsx
@@ -1,4 +1,5 @@
-import { useEffect, useState, useRef, useCallback } from 'react'
+import { useEffect, useState, useRef } from 'react'
+import useLayoutEffect from '../../hooks/useIsomorphicLayoutEffect';
 import { gsap, ScrollTrigger } from '../../config/gsapConfig';
 import classes from './MobileNavigation.module.css'
 import classNames from 'classnames';
@@ -6,6 +7,9 @@ import classNames from 'classnames';
 let cx = classNames.bind(classes);
 
 const MobileNavigation = () => {
+    const scopeRef = useRef();
+    const timeline = useRef();
+
     const navRef = useRef(null);
     const buttonRef = useRef(null);
 
@@ -19,95 +23,85 @@ const MobileNavigation = () => {
 
     const openMenu = () => { setIsOpen(true) };
     const closeMenu = () => { setIsOpen(false) };
-
-    
    
+    useLayoutEffect(() => {
+
+        const ctx = gsap.context(() => {
+            /* Menu Open/Close Timeline */
+            timeline.current = gsap.timeline({
+                defaults: {duration: 0.04},
+                onUpdate: () => { }
+            })
+            .addLabel("start", 0)
+            .fromTo(buttonRef.current, {
+                "--button-before-y": "50%",
+                "--button-before-rotation": "45deg",
+                "--button-after-y": "50%",
+                "--button-after-rotation": "-45deg",
+                "--button-scale": 0,
+                "--button-shadow-blur": "16px",
+                "--button-shadow-spread": "20px",
+                "--button-shadow-inner-spread": "9px",
+                "--button-shadow-color": "rgba(52, 84, 209, 0.5)"
+            },{
+                "--button-before-y": "50%",
+                "--button-before-rotation": "45deg",
+                "--button-after-y": "50%",
+                "--button-after-rotation": "-45deg",
+                "--button-scale": 1,
+                "--button-shadow-blur": "0px",
+                "--button-shadow-spread": "1200px",
+                "--button-shadow-inner-spread": "20px",
+                "--button-shadow-color": "rgba(0, 0, 0, 0.9)"
+            })
+            .fromTo(".navItem", {
+                "--nav-item-scale": 0.2
+            }, {
+                "--nav-item-scale": 1,
+                duration: 0.2
+            }, "start+=0.1")
+            .fromTo([testimonialsRef.current, contactRef.current], {
+                "--nav-item-opacity": 0
+            }, {
+                "--nav-item-opacity": 1,
+                duration: 0.4
+            })
+            .progress(0).pause();
+
+            /* Sticky Toggle Trigger */
+
+            const setFixed = () => gsap.set([navRef.current, buttonRef.current], {
+                position: 'fixed'
+            });
+        
+            const setAbsolute = () => gsap.set([navRef.current, buttonRef.current], {
+                position: 'absolute'
+            });
+
+            ScrollTrigger.create({
+                trigger: document.querySelector('#about'),
+                start: "top top",
+                end: 99999,
+                onEnter: () => setFixed(),
+                onLeaveBack: () => setAbsolute()
+            })
+        }, scopeRef);
+
+        return () => ctx.revert();
+    }, [])
+
     useEffect(() => {
-        var menuTimeline = gsap.timeline({
-            defaults: {duration: 0.04},
-            onUpdate: () => { }
-        })
-        .addLabel("start", 0)
-        .fromTo(buttonRef.current, {
-            "--button-before-y": "50%",
-            "--button-before-rotation": "45deg",
-            "--button-after-y": "50%",
-            "--button-after-rotation": "-45deg",
-            "--button-scale": 0,
-            "--button-shadow-blur": "16px",
-            "--button-shadow-spread": "20px",
-            "--button-shadow-inner-spread": "9px",
-            "--button-shadow-color": "rgba(52, 84, 209, 0.5)"
-        },{
-            "--button-before-y": "50%",
-            "--button-before-rotation": "45deg",
-            "--button-after-y": "50%",
-            "--button-after-rotation": "-45deg",
-            "--button-scale": 1,
-            "--button-shadow-blur": "0px",
-            "--button-shadow-spread": "1200px",
-            "--button-shadow-inner-spread": "20px",
-            "--button-shadow-color": "rgba(0, 0, 0, 0.9)"
-        })
-        // .fromTo(aboutRef.current, {transitionDelay: '0s'}, {transitionDelay: '2s'}, "start")
-        // .fromTo(workRef.current, {transitionDelay: '0.5s'}, {transitionDelay: '0.15s'}, "start")
-        // .fromTo(experienceRef.current, {transitionDelay: '0.1s'}, {transitionDelay: '0.1s'}, "start")
-        // .fromTo(testimonialsRef.current, {transitionDelay: '0.15s'}, {transitionDelay: '0.5s'}, "start")
-        // .fromTo(contactRef.current, {transitionDelay: '0.2s'}, {transitionDelay: '0s'}, "start")
-        // .fromTo(navRef.current, // causing weird offset of hamburger
-        //     {y: 0}, {y: 35, duration: 1}, "start+=0.1"
-        // )
-        .fromTo(".navItem", {
-            "--nav-item-scale": 0.2
-        }, {
-            "--nav-item-scale": 1,
-            duration: 0.2
-        }, "start+=0.1")
-        .fromTo([testimonialsRef.current, contactRef.current], {
-            "--nav-item-opacity": 0
-        }, {
-            "--nav-item-opacity": 1,
-            duration: 0.4
-        })
-        .progress(0).pause();
-
-        if (isOpen && !menuTimeline.isActive()) {
-            // console.log('isOpen: ', isOpen, ' activeTimeline: ', menuTimeline.isActive())
-            // console.log('progress: ', menuTimeline.progress())
-            menuTimeline.play();
-            // menuTimeline.pause();
-        } else if (!isOpen && menuTimeline.progress() === 1) {
-            // console.log('isOpen: ', isOpen)
-            // console.log('isActive: ', menuTimeline.isActive())
-            // console.log('progress: ', menuTimeline.progress())
-            menuTimeline.reverse(0);
+        if (isOpen && !timeline.current.isActive()) {
+            timeline.current.play();
+        } else if (!isOpen && timeline.current.progress() === 1) {
+            timeline.current.reverse(0);
         }
-
-
-        /* Sticky Toggle Trigger */
-
-        const setFixed = () => gsap.set([navRef.current, buttonRef.current], {
-            position: 'fixed'
-        });
-    
-        const setAbsolute = () => gsap.set([navRef.current, buttonRef.current], {
-            position: 'absolute'
-        });
-
-        ScrollTrigger.create({
-            trigger: document.querySelector('#about'),
-            start: "top top",
-            end: 99999,
-            onEnter: () => setFixed(),
-            onLeaveBack: () => setAbsolute()
-        })
-
     }, [isOpen])
 
     
 
     return (
-        <>
+        <div ref={scopeRef}>
             <input 
                 id="mobile-menu-toggle" 
                 className={classes.toggle} 
@@ -126,7 +120,7 @@ const MobileNavigation = () => {
                 <a ref={testimonialsRef} className={cx('navItem', 'font-play',classes.mobileNavItem)} href="#testimonials" onClick={closeMenu}><strong>Testimonials</strong></a>
                 <a ref={contactRef} className={cx('navItem', 'font-play',classes.mobileNavItem)} href="#contact" onClick={closeMenu}><strong>Contact</strong></a>
             </nav>
-        </>
+        </div>
     )
 }
 
